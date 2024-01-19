25c401029d145acfc0de04eca0f55e33a6f9f3e4
DmitryNeganov <glorynewfound@yandex.ru>
2024-01-19 16:58:32 +0300

    2095. Delete the Middle Node of a Linked List

---
+++  DeleteTheMiddleNodeOfALinkedList.go
@@ -0,0 +1,29 @@
+package main
+
+import "ftw"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func deleteMiddle(head *ListNode) *ListNode {
+
+    if head == nil || head.Next == nil {
+        return nil
+    }
+
+    fast := head.Next.Next
+    slow := head
+
+    for fast != nil && fast.Next != nil {
+        fast = fast.Next.Next
+        slow = slow.Next
+    }
+
+    slow.Next = slow.Next.Next
+
+    return head
+}
