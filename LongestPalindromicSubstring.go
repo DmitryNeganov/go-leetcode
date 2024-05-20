25fe934b8e9feaf8db08cbed67039de78978b1e0
Dmitry Neganov <102044344+DmitryNeganov@users.noreply.github.com>
2024-05-20 13:54:32 +0300

    Update LongestPalindromicSubstring.go

--- LongestPalindromicSubstring.go
+++ LongestPalindromicSubstring.go
@@ -22,6 +22,45 @@ func longestPalindromeSlow(s string) string {
 	return result
 }
 
+// O(N2)
+func longestPalindromeFaster(s string) string {
+
+    n := len(s)
+
+	iAns := 0
+	jAns := 0
+
+    dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+    
+    for i := 0; i < n; i++ {
+        dp[i][i] = true;
+    }
+
+    for i := 0; i < n - 1; i++ {
+		dp[i][i+1] = (s[i] == s[i + 1])
+        if dp[i][i+1] {
+			iAns = i
+			jAns = i + 1
+		}
+    }
+
+	for dif := 2; dif < n; dif++ {
+		for i := 0; i < n - dif ; i++ {
+			j := i + dif
+			dp[i][j] = (s[i] == s[j]) && (dp[i+1][j-1])
+			if dp[i][j] {
+				iAns = i
+				jAns = j
+			}
+		}
+	}
+
+	return s[iAns : jAns + 1]
+}
+
 // O(N)
 func checkPalindromSlow(s string) bool {
 	left := 0
