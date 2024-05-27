89cdf0516a8c636488905a16d2a45fbd879a4a67
Dmitry Neganov <102044344+DmitryNeganov@users.noreply.github.com>
2024-05-27 12:34:16 +0300

    Create TimeNeededToBuyTickets.go

---
+++ TimeNeededToBuyTickets.go
@@ -0,0 +1,19 @@
+func timeRequiredToBuy(tickets []int, k int) int {
+    counter := 0
+
+    i := 0
+    for tickets[k] > 0 {
+        current := tickets[i]
+        if current > 0 {
+            counter++
+            tickets[i] = current - 1
+        }
+        if i == len(tickets) - 1 {
+            i = 0
+        } else {
+            i++
+        }
+    }
+
+    return counter
+}
